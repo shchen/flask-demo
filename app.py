48c7b4b7c16ab9ec2d3320f47f720d8254dccf76
Shih-Hua Chen <shchen0304@gmail.com>
2015-09-01 18:47:05 -0700

    Checkbox test

--- app.py
+++ app.py
@@ -1,14 +1,23 @@
 from flask import Flask, render_template, request, redirect
 
-app = Flask(__name__)
+app_stock = Flask(__name__)
 
-@app.route('/')
+app_stock.vars = {}
+
+@app_stock.route('/')
 def main():
-  return redirect('/index')
+    return redirect('/index')
 
-@app.route('/index')
+@app_stock.route('/index', method=['GET', 'POST'])
 def index():
-  return render_template('index.html')
+    if request.method == 'GET':
+        return render_template('index.html')
+    else:
+        app_stock.vars['ticker'] = request.form['ticker']
+        print request.form['features']
+        app_stock.vars['features'] = request.form['features']
+
+    return "POST method!"
 
 if __name__ == '__main__':
-  app.run(port=33507)
+  app_stock.run(port=33507)
