81d5d10de63a7b40386f6d22a5287ab74865af35
Shih-Hua Chen <shchen0304@gmail.com>
2015-09-02 13:55:39 -0700

    have graph/error page rendering, milestone project completed

--- app.py
+++ app.py
@@ -6,7 +6,6 @@ from bokeh.resources import INLINE
 from bokeh.templates import RESOURCES
 from bokeh.util.string import encode_utf8
 
-import numpy as np
 import requests
 import pandas as pd
 
@@ -14,10 +13,19 @@ app_stock = Flask(__name__)
 
 app_stock.vars = {}
 
+app_stock.vars['color'] = {
+    'Close': 'navy',
+    'Adj. Close': 'orange',
+    'Volume': 'green'
+}
+
 @app_stock.route('/')
 def main():
     return redirect('/index')
 
+@app_stock.route('/error-quandle')
+def error_quandle():
+    return render_template('error.html')
 
 @app_stock.route('/index', methods=['GET', 'POST'])
 def index():
@@ -32,31 +40,46 @@ def index():
         # Pull stock data
         url = 'https://www.quandl.com/api/v3/datasets/WIKI/' + app_stock.vars['ticker'] + '/data.json'
         r = requests.get(url)
-        data = r.json()['dataset_data']['data']
-        cols = r.json()['dataset_data']['column_names']
-        df = pd.DataFrame(data, columns=cols)
-        #print df[['Date'] + app_stock.vars['features']].head(5)
+        if r.status_code == 404:
+            return redirect('/error-quandle')
+        else:
+            data = r.json()['dataset_data']['data']
+            cols = r.json()['dataset_data']['column_names']
+            app_stock.vars['data'] = pd.DataFrame(data, columns=cols)
+            #app_stock.vars['data'] = df[['Date'] + app_stock.vars['features']].head(5)
+            return redirect('/graph')
 
-        # Create a line plot from our data.
-        p = figure(width=700, height=500, x_axis_type="datetime",
-                   title="Data from Quandle WIKI set")
-        for category in app_stock.vars['features']:
-            p.line(pd.to_datetime(df['Date']), df[category], color='navy', alpha=0.5)
 
-        # Configure resources to include BokehJS inline in the document.
-        plot_resources = RESOURCES.render(
-            js_raw=INLINE.js_raw,
-            css_raw=INLINE.css_raw,
-            js_files=INLINE.js_files,
-            css_files=INLINE.css_files,
-        )
+@app_stock.route('/graph', methods=['GET'])
+def graph():
+    df = app_stock.vars['data']
 
-        script, div = components(p, INLINE)
-        html = render_template(
-            'graph.html',
-            plot_script=script, plot_div=div, plot_resources=plot_resources
-        )
+    # Create a line plot from our data.
+    p = figure(width=700, height=500, x_axis_type="datetime",
+                title="Data from Quandle WIKI set")
+    for category in app_stock.vars['features']:
+        p.line(pd.to_datetime(df['Date']), df[category],
+                color=app_stock.vars['color'][category], line_width=1,
+                legend=app_stock.vars['ticker'] + ": " + category)
+
+    p.legend.orientation = "top_right"
+
+    # Configure resources to include BokehJS inline in the document.
+    plot_resources = RESOURCES.render(
+        js_raw=INLINE.js_raw,
+        css_raw=INLINE.css_raw,
+        js_files=INLINE.js_files,
+        css_files=INLINE.css_files,
+    )
+
+    script, div = components(p, INLINE)
+    html = render_template(
+        'graph.html',
+        ticker=app_stock.vars['ticker'],
+        plot_script=script, plot_div=div, plot_resources=plot_resources
+    )
     return encode_utf8(html)
+
 
 
 if __name__ == '__main__':
